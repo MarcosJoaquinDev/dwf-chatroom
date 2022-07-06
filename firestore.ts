a49f344c1af9a39ad1141b9a9a49e0ac3285c005
Marcos Acuña <marcosjuako@hotmail.com>
2022-07-06 20:24:06 -0300

    key firebase

--- firestore.ts
+++ firestore.ts
@@ -1,12 +1,11 @@
-import * as admin from "firebase-admin";
-import * as serviceAccount from "./key.json";
+import * as admin from 'firebase-admin';
+const DATA_KEY = JSON.parse(process.env.FIREBASE_KEY);
 
 admin.initializeApp({
-
-  credential: admin.credential.cert(serviceAccount as any ),
-    databaseURL: "https://dwf-m6-mj-dev-default-rtdb.firebaseio.com"
+	credential: admin.credential.cert(DATA_KEY as any),
+	databaseURL: 'https://dwf-m6-mj-dev-default-rtdb.firebaseio.com',
 });
 const DATA_BASE = admin.firestore();
 const RTDB = admin.database();
 
-export {DATA_BASE, RTDB}
+export { DATA_BASE, RTDB };
